ff5a7555558c64657f129aeeb462af2eb721f6ae
Bastien Cerri <139891786+bcerripromethee@users.noreply.github.com>
2025-07-24 10:30:26 +0200

    Add parametrisation mean, min, max tests cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,6 +6,16 @@ import pytest
 
 from inflammation.models import daily_mean, daily_max, daily_min
 
+@pytest.mark.parametrize("test, expected",
+                         [
+                             ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+                             ([ [1, 2], [3, 4], [5, 6] ], [3, 4])
+                         ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
 
@@ -41,17 +51,14 @@ def test_daily_max_zeros():
 
     npt.assert_array_almost_equal(daily_max(test_input), test_result)
 
-
-def test_daily_max_integers():
+@pytest.mark.parametrize("test, expected", [
+                             ([ [1, 50, 0], [522, 10, 0], [10, 10, 0] ], [522, 50, 0]),
+                             ([[88, 99, 0], [1, -1, 15], [55, 63, 0]], [88, 99, 15])
+                         ])
+def test_daily_max_integers(test, expected):
     """Test that max function works for an array of integers."""
 
-    test_input = np.array([[1, 2, 3],
-                           [4, 50, 6],
-                           [7, 8, 9]])
-    
-    test_result = np.array([7, 50, 9])
-
-    npt.assert_array_almost_equal(daily_max(test_input), test_result)
+    npt.assert_array_almost_equal(daily_max(np.array(test)), np.array(expected))
 
 
 def test_daily_min_zeros():
@@ -65,17 +72,14 @@ def test_daily_min_zeros():
 
     npt.assert_array_almost_equal(daily_min(test_input), test_result)
 
-
-def test_daily_min_integers():
+@pytest.mark.parametrize("test, expected", [
+                             ([ [1, 50, 0], [522, 10, 0], [10, 10, 0] ], [1, 10, 0]),
+                             ([[88, 99, 0], [1, -1, 15], [55, 63, 0]], [1, -1, 0])
+                         ])
+def test_daily_min_integers(test, expected):
     """Test that max function works for an array of integers."""
 
-    test_input = np.array([[11, 2, 0],
-                           [3, 40, 1],
-                           [51, 6, 13]])
-    
-    test_result = np.array([3, 2, 0])
-
-    npt.assert_array_almost_equal(daily_min(test_input), test_result)
+    npt.assert_array_almost_equal(daily_min(np.array(test)), np.array(expected))
 
 
 def test_daily_min_string():
